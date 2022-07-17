152dab43aaf1398b9eced58974608ca3faf868d4
James Foster <9198424+JPFoz@users.noreply.github.com>
2022-07-17 12:01:17 +0200

    Add better readme and a copy fix (#1)

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -18,7 +18,7 @@ const firebaseConfig = {
     apiKey: process.env.FIRESTORE_API_KEY,
     authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
     projectId: process.env.FIRESTORE_PROJECT_ID,
-    storageBucket: process.env.FIRESTORE_PROJECT_ID,
+    storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
     appId: process.env.FIRESTORE_APP_ID,
 };
 
